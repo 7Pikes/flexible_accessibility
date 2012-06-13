245ad1195ecdc5868ef0c5f80c4d1c95e2b9d306
Sergey Awanesov <sergey.awanesov@gmail.com>
2012-06-13 12:32:58 +0400

    Changes in controller methods and moved version to pre

--- lib/flexible_accessibility/controller_methods.rb
+++ lib/flexible_accessibility/controller_methods.rb
@@ -2,6 +2,11 @@ module FlexibleAccessibility
   module ControllerMethods
   	module ClassMethods
       before_filter :check_access_to_resource
+      before_filter :check_if_authorized
+
+      def skip_authorization_on_resource
+        @authorized = true
+      end
 
   	  def authorize actions=[]
   	  	@actions = actions
@@ -14,8 +19,12 @@ module FlexibleAccessibility
 
       def check_access_to_resource
         if @actions.include current_action[1].to_sym
-          raise FlexibleAccessibility::AccessDeniedException unless Permission.check_access "#{current_action[0]}##{current_action[1]}", current_action
+          @authorized = true unless Permission.check_access "#{current_action[0]}##{current_action[1]}", current_action
         end
+      end
+
+      def check_if_authorized
+        raise FlexibleAccessibility::AccessDeniedException unless @authorized
       end
       
       def has_access? controller, action
