9a80b4719eac564cceea658bd3403beded9af75c
Sergey Awanesov <sergey.awanesov@gmail.com>
2012-07-01 12:49:36 +0500

    Changes in inti

--- lib/flexible_accessibility.rb
+++ lib/flexible_accessibility.rb
@@ -1,5 +1,2 @@
-require 'lib/flexible_accessibility/controller_methods.rb'
-require 'lib/flexible_accessibility/exceptions.rb'
-
-module FlexibleAccessibility
-end
+require 'flexible_accessibility/controller_methods.rb'
+require 'flexible_accessibility/exceptions.rb'
