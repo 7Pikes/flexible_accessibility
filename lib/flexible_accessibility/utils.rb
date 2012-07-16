23ff4424c9b30ea0c1fe0ed40330d36495a1a51e
Sergey Awanesov <sergey.awanesov@gmail.com>
2012-07-16 13:10:30 +0400

    Fix

---
+++ lib/flexible_accessibility/utils.rb
@@ -0,0 +1,22 @@
+module FlexibleAccessibility
+  class Utils
+    def initialize
+      @path = "#{::Rails.root}/app/controllers/"
+      @controllers = []
+    end
+
+    def get_controllers
+      get_controllers_recursive @path
+    end
+
+    def get_controllers_recursive path
+      (Dir.new(path).entries - ["..", "."]).each do |entry|
+        if File.directory? path + entry
+          get_controllers_recursive path + entry + '/'
+        else
+          @controllers << File.basename(path + entry) if File.extname(path + entry) == '.rb'
+        end
+      end
+    end
+  end
+end
