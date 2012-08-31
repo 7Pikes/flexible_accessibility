a4bcfbefabe961350d7151ad3be8e4fa54a62484
mochnatiy <sergey.awanesov@gmail.com>
2012-08-31 13:25:49 +0400

    Fixed error in collecting namespaces

--- lib/flexible_accessibility/permission.rb
+++ lib/flexible_accessibility/permission.rb
@@ -9,23 +9,22 @@ module FlexibleAccessibility
     end
 
   	class << self
-	  def all
-		permissions = []
-		Utils.new.get_controllers.each do |klass|
-		  permissions << Permission.new(:controller => klass.gsub(/_controller/, "").to_sym, :actions => klass.camelize.constantize.instance_variable_get(:@_checkable_routes))
+		  def all
+				permissions = []
+				Utils.new.get_controllers.each do |klass|
+				  permissions << Permission.new(:controller => klass.gsub(/_controller/, "").to_sym, :actions => klass.camelize.constantize.instance_variable_get(:@_checkable_routes))
+				end
+				permissions
+	  	end
+
+		  def is_action_permitted? permission
+		  	self.is_action_permitted_for_user? permission, current_user
+		  end
+
+		  def is_action_permitted_for_user? permission, user
+		  	# TODO: Avoid these code, maybe handle a callback included in application
+		  	!AccessRule.where(["permission = ? and user_id = ?", permission, user.id]).empty?
+		  end
 		end
-		permissions
-	  end
-
-      # Stub methods
-	  def is_action_permitted? permission
-	  	self.is_action_permitted_for_user? permission, current_user
-	  end
-
-	  def is_action_permitted_for_user? permission, user
-	  	# TODO: Avoid these code, maybe handle a callback included in application
-	  	!AccessRule.where(["permission = ? and user_id = ?", permission, user.id]).empty?
-	  end
-	end
   end
 end
--- lib/flexible_accessibility/utils.rb
+++ lib/flexible_accessibility/utils.rb
@@ -9,14 +9,15 @@ module FlexibleAccessibility
       get_controllers_recursive @path
     end
 
+    # All controller clases placed in :default scope
     def get_controllers_recursive path
       (Dir.new(path).entries - ["..", "."]).each do |entry|
         if File.directory? path + entry
           get_controllers_recursive path + entry + '/'
         else
           parent_directory = File.dirname(path + entry).split(/\//).last
-          puts parent_directory
           container = parent_directory == "controllers" ? "default" : parent_directory
+          @controllers[container.to_sym] = [] unless @controllers.has_key? container.to_sym
           @controllers[container.to_sym] << File.basename(path + entry, ".*") unless File.basename(path + entry, ".*") == "application_controller"
         end
       end
