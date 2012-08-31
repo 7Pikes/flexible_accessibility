7755e3eb813d4fc62e1a068e0e9ce89462d9f4a3
mochnatiy <sergey.awanesov@gmail.com>
2012-08-31 12:25:55 +0400

    Test

--- lib/flexible_accessibility/utils.rb
+++ lib/flexible_accessibility/utils.rb
@@ -14,8 +14,8 @@ module FlexibleAccessibility
         if File.directory? path + entry
           get_controllers_recursive path + entry + '/'
         else
-          puts parent_directory
           parent_directory = File.dirname(path + entry).split(/\//).last
+          puts parent_directory
           container = parent_directory == "controllers" ? "default" : parent_directory
           @controllers[container.to_sym] << File.basename(path + entry, ".*") unless File.basename(path + entry, ".*") == "application_controller"
         end
