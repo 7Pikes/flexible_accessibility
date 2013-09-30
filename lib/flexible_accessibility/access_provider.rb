e55ffe2e855e9ff98137e7b6ec378db6b4ab972d
Sergey Avanesov <sergey.awanesov@gmail.com>
2013-09-30 11:54:21 +0400

    Preload permissions

--- lib/flexible_accessibility/access_provider.rb
+++ lib/flexible_accessibility/access_provider.rb
@@ -8,6 +8,7 @@ module FlexibleAccessibility
       end
 
       def is_action_permitted_for_user?(permission, user)
+        preload_permissions(user)
         user.instance_variable_get(:@_available_permissions).include? permission
       end
     end
