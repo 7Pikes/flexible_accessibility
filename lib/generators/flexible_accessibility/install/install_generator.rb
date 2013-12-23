213b76c2c57b159045885a27bbbd5388e8bbed86
Sergey Avanesov <sergey.awanesov@gmail.com>
2013-12-23 15:23:55 +0400

    Fixed bad code in install generator

--- lib/generators/flexible_accessibility/install/install_generator.rb
+++ lib/generators/flexible_accessibility/install/install_generator.rb
@@ -5,19 +5,19 @@ module FlexibleAccessibility
     class InstallGenerator < ::Rails::Generators::Base
       include Rails::Generators::Migration
       source_root File.expand_path('../templates', __FILE__)
-      desc "Add the migrations"
+      desc 'Add the migrations'
 
       def self.next_migration_number(path)
-        unless @prev_migration_nr
-          @prev_migration_nr = Time.now.utc.strftime("%Y%m%d%H%M%S").to_i
-        else
+        if @prev_migration_nr
           @prev_migration_nr += 1
+        else
+          @prev_migration_nr = Time.now.utc.strftime('%Y%m%d%H%M%S').to_i
         end
         @prev_migration_nr.to_s
       end
 
       def copy_migrations
-        migration_template "create_access_rules.rb", "db/migrate/create_access_rules.rb"
+        migration_template 'create_access_rules.rb', 'db/migrate/create_access_rules.rb'
       end
     end
   end
