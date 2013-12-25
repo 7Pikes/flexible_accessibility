73404e3e23c7a874750547a9808937350caa8c51
Sergey Avanesov <sergey.awanesov@gmail.com>
2013-12-25 12:18:32 +0500

    Owner -> owner_id

--- lib/generators/flexible_accessibility/install/templates/create_access_rules.rb
+++ lib/generators/flexible_accessibility/install/templates/create_access_rules.rb
@@ -2,10 +2,10 @@ class CreateAccessRules < ActiveRecord::Migration
   def self.up
     create_table :access_rules do |t|
       t.string  :permission
-      t.integer :owner    
+      t.integer :owner_id
       t.timestamps
 
-      t.index [:owner], :name => "access_rules_index_on_owner"
+      t.index [:owner_id], :name => 'access_rules_index_on_owner_id'
     end
   end
 
