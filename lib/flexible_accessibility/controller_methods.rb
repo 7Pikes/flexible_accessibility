444477e2daa3c26ea8b4301e28435eda4ba11d5d
Sergey Avanesov <sergey.awanesov@gmail.com>
2013-12-25 12:21:15 +0500

    not_authorize macro and backward compatibility

--- lib/flexible_accessibility/controller_methods.rb
+++ lib/flexible_accessibility/controller_methods.rb
@@ -2,11 +2,16 @@ module FlexibleAccessibility
   module ControllerMethods
   	module ClassMethods
 
+      # Compatibility with previous versions
+      def skip_authorization_here
+        not_authorize :all
+      end
+
       # Macro for define actions without authorization
-      def skip_authorization(args={})
+      def not_authorize(args={})
         valid_arguments = parse_and_validate_arguments(args)
         self.instance_variable_set(:@_non_verifiable_routes, self.action_methods) if valid_arguments[:all]
-        self.instance_variable_set(:@_non_verifiable_routes, valid_arguments[:on]) unless valid_arguments[:on].nil?
+        self.instance_variable_set(:@_non_verifiable_routes, valid_arguments[:only]) unless valid_arguments[:only].nil?
         self.instance_variable_set(:@_verifiable_routes, []) if self.instance_variable_get(:@_non_verifiable_routes).nil?
       end
 
@@ -17,15 +22,14 @@ module FlexibleAccessibility
         self.instance_variable_set(:@_verifiable_routes, self.action_methods - valid_arguments[:except]) unless valid_arguments[:except].nil?
         self.instance_variable_set(:@_verifiable_routes, self.action_methods) if valid_arguments[:all]
         self.instance_variable_set(:@_non_verifiable_routes, []) if self.instance_variable_get(:@_non_verifiable_routes).nil?
-        # TODO: get info from routes
   	  end
 
       private
       # Parse arguments from macro calls
       def parse_and_validate_arguments(args={})
         result = {}
-        (result[:all] = true) and return result if args.to_s == "all" || args.to_s == "everywhere"
-        [:on, :only, :except].each do |key|
+        (result[:all] = true) and return result if args.to_s == 'all'
+        [:only, :except].each do |key|
           unless args[key].nil?
             raise ActionsValueException unless args[key].instance_of?(Array)
             result[key] = args[key].map!{ |v| v.to_s }.to_set
